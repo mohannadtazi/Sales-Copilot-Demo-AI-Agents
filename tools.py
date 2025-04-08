d7d7d3982aa97bba0089874926dd3c285b81f554
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2025-04-08 15:53:05 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -9,12 +9,5 @@ from langchain.utilities import GoogleSerperAPIWrapper
 llm = LLM(model="huggingface/mistralai/Mistral-7B-Instruct-v0.1", api_key=os.getenv('HF_API_KEY'))
 #scraper_tool = FirecrawlScrapeWebsiteTool(url='firecrawl.dev',   params={ 'limit': 3,  'scrapeOptions': {'formats': ['markdown', 'html']}  })
 
-scraper_tool = ScrapeWebsiteTool(llm = llm)
-search = GoogleSerperAPIWrapper(api_key=os.getenv('SERPAPI_API_KEY'))
-
-search_tool = Tool(
-    name="Scrape google searches",
-    func=search.run,
-    description="useful for when you need to ask the agent to search the internet",
-)
-#search_tool = SerperDevTool(api_key=os.getenv('SERPAPI_API_KEY'))
+scraper_tool = FirecrawlScrapeWebsiteTool(api_key=userdata.get('FIRECRAWL_API_KEY'))
+search_tool = SerperDevTool(api_key=userdata.get('SERPAPI_API_KEY'))
