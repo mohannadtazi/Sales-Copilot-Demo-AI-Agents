22e9f199cef82061750ff7e470682b59e72de13e
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2025-04-08 14:51:10 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,9 +2,11 @@ from crewai_tools import SerperDevTool, FirecrawlScrapeWebsiteTool
 import os
 from dotenv import load_dotenv
 load_dotenv() 
+from crewai import LLM
 
-scraper_tool = FirecrawlScrapeWebsiteTool(url='firecrawl.dev',   params={
-    'limit': 3, 
-    'scrapeOptions': {'formats': ['markdown', 'html']}
-  })
+
+llm = LLM(model="huggingface/mistralai/Mistral-7B-Instruct-v0.1", api_key=os.getenv('HF_API_KEY'))
+#scraper_tool = FirecrawlScrapeWebsiteTool(url='firecrawl.dev',   params={ 'limit': 3,  'scrapeOptions': {'formats': ['markdown', 'html']}  })
+
+scraper_tool = ScrapeWebsiteTool(llm = llm)
 search_tool = SerperDevTool(api_key=os.getenv('SERPAPI_API_KEY'))
