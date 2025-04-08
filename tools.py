8fc9e2594f7bd74fa09117e13ca53349596a6212
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2025-04-08 14:29:58 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 scraper_tool = FirecrawlScrapeWebsiteTool(url='firecrawl.dev',   params={
-    'limit': 100, 
+    'limit': 3, 
     'scrapeOptions': {'formats': ['markdown', 'html']}
   })
 search_tool = SerperDevTool(api_key=os.getenv('SERPAPI_API_KEY'))
