e4f184d52ff92933d88ea2a9ee0039305d3ed0b9
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2025-04-08 15:22:44 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -4,9 +4,17 @@ from dotenv import load_dotenv
 load_dotenv() 
 from crewai import LLM
 
+from langchain.utilities import GoogleSerperAPIWrapper
 
 llm = LLM(model="huggingface/mistralai/Mistral-7B-Instruct-v0.1", api_key=os.getenv('HF_API_KEY'))
 #scraper_tool = FirecrawlScrapeWebsiteTool(url='firecrawl.dev',   params={ 'limit': 3,  'scrapeOptions': {'formats': ['markdown', 'html']}  })
 
 scraper_tool = ScrapeWebsiteTool(llm = llm)
-search_tool = SerperDevTool(api_key=os.getenv('SERPAPI_API_KEY'))
+search = GoogleSerperAPIWrapper(api_key=os.getenv('SERPER_API_KEY'))
+
+search_tool = Tool(
+    name="Scrape google searches",
+    func=search.run,
+    description="useful for when you need to ask the agent to search the internet",
+)
+#search_tool = SerperDevTool(api_key=os.getenv('SERPAPI_API_KEY'))
